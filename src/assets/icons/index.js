70c4180c43a14994a2c8344c79c0cd401dd16c7e
AnshulGaurwebd <anshulgaur003@gmail.com>
2024-11-01 20:10:06 +0530

    first commit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,44 @@
+import css from "./css.svg";
+
+import github from "./github.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import linkedin from "./linkedin.svg";
+
+import car from "./car.svg";
+
+import summiz from "./summiz.svg";
+
+import arrow from "./arrow.svg";
+import contact from "./contact.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+
+export {
+  css,
+  github,
+  html,
+  javascript,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  linkedin,
+  car,
+  summiz,
+  arrow,
+  contact,
+  soundon,
+  soundoff,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,113 @@
+import {
+  car,
+  contact,
+  css,
+  github,
+  html,
+  javascript,
+  linkedin,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  summiz,
+  tailwindcss,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/AnshulGaurwebd",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "www.linkedin.com/in/anshul-gaur-22b754249",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: car,
+    theme: "btn-back-red",
+    name: "Blog Website",
+    description:
+      "Developed a web application for blogging and travel to influence or to give advices what to visit and when to visit.",
+    link: "https://a-blog-site.netlify.app",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-green",
+    name: "Iphone-pro promotion",
+    description: "Created a promotional website for apple iphone pro.",
+    link: "https://github.com/AnshulGaurwebd/Apple-iphone-pro.git",
+  },
+];
